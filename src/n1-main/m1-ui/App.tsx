a897b52880051011703c16e4b745d4ef1a142dda
highhack <forgreatperson@gmail.com>
2021-05-13 20:49:36 +0300

    add fetch Sergey

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import Header from "./header/Header";
 import Routes from "./routes/Routes";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../m2-bll/store";
+import {initializeAppTC, RequestStatusType} from "../m2-bll/appReducer";
 
 
 const App = () => {
+
+  // const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized)
+  // const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+  // let dispatch = useDispatch()
+
+  // useEffect(() => {
+  //   dispatch(initializeAppTC())}, [])
+  //
+  // if (!isInitialized) {
+  //   return <div>
+  //      Loading...
+  //   </div>
+  // }
+
   return (
       <BrowserRouter>
     <div className="App">
